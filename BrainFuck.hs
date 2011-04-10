318e331d2e0cf6a6e1a169de5142687a7f1a1a94
nike <hismatovns@gmail.com>
2011-04-10 16:41:04 +0600

    output oparator

--- BrainFuck.hs
+++ BrainFuck.hs
@@ -33,16 +33,19 @@ fwd b
 
 back :: B -> IO B
 back b
-    | pos b > 0     = return $ B (-1 + pos b) (cells b)
-    | otherwise     = return $ B (-1 + size b) (cells b)
+    | pos b > 0 = return $ B (-1 + pos b) (cells b)
+    | otherwise = return $ B (-1 + size b) (cells b)
 
 
+output :: B -> IO B
+output b = do { print $ (cells b) !! (pos b); return b }
 
 eval :: [Char] -> IO B
-eval s = foldl op initial_machine s
+eval s = foldl (\machine opcode->machine >>= (get_op opcode)) initial_machine s
             where initial_machine = createB 30
-                  op machine opcode = case opcode of
-                                          '+' -> machine >>= inc
-                                          '-' -> machine >>= dec
-                                          '>' -> machine >>= fwd
-                                          '<' -> machine >>= back
+                  get_op opcode = case opcode of
+                                      '+' -> inc
+                                      '-' -> dec
+                                      '>' -> fwd
+                                      '<' -> back
+                                      '.' -> output
