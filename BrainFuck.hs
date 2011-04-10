96d47fa13a4e05cca4059b5ba4c2bbb266b7320c
nike <hismatovns@gmail.com>
2011-04-10 20:25:24 +0600

    ',' - input operator

--- BrainFuck.hs
+++ BrainFuck.hs
@@ -1,24 +1,12 @@
 module BrainFuck where
 
+import Data.Char
+
 data B = B { pos :: Int, cells :: [Int] } deriving (Show)
 
 createB :: Int -> IO B
 createB n = return $ B 0 (fillzeros(n))
                 where fillzeros n = take n $ repeat 0
-
-
-b_current_cell_op :: B -> (Int -> Int) -> B
-b_current_cell_op b f = B n ((take n xs) ++ [f $ xs !! n] ++ (drop (n + 1) xs))
-                        where xs = cells b
-                              n  = pos b
-
-
-inc :: B -> IO B
-inc b = return $ b_current_cell_op b (+1)
-
-
-dec :: B -> IO B
-dec b = return $ b_current_cell_op b (+(-1))
 
 
 size :: B -> Int
@@ -37,15 +25,52 @@ back b
     | otherwise = return $ B (-1 + size b) (cells b)
 
 
+get_curr :: B -> Int
+get_curr b = (cells b) !! (pos b)
+
+
+set_curr :: B -> Int -> B
+set_curr b val = B n ((take n xs) ++ [val] ++ (drop (n + 1) xs))
+                    where xs = cells b
+                          n  = pos b
+
+
+inc :: B -> IO B
+inc b = return $ set_curr b new_val
+            where new_val = 1 + (get_curr b)
+
+
+dec :: B -> IO B
+dec b = return $ set_curr b new_val
+            where new_val = -1 + (get_curr b)
+
+
 output :: B -> IO B
-output b = do { print $ (cells b) !! (pos b); return b }
+output b = (putStr [(chr $ (cells b) !! (pos b))]) >> return b
+
+
+input :: B -> IO B
+input b = (getLine >>= return . ord . head) >>= \x->return $ set_curr b x
+
+
+noop :: B -> IO B
+noop b = return b
+
+
+command :: Char -> (B -> IO B)
+command '+' = inc
+command '-' = dec
+command '>' = fwd
+command '<' = back
+command '.' = output
+command ',' = input
+command _   = noop
+
+
+eval_with_initial :: [Char] -> IO B -> IO B
+eval_with_initial s initial_machine = foldl (\machine opcode->machine >>= (command opcode)) initial_machine s
+
 
 eval :: [Char] -> IO B
-eval s = foldl (\machine opcode->machine >>= (get_op opcode)) initial_machine s
-            where initial_machine = createB 30
-                  get_op opcode = case opcode of
-                                      '+' -> inc
-                                      '-' -> dec
-                                      '>' -> fwd
-                                      '<' -> back
-                                      '.' -> output
+eval s = eval_with_initial s (createB 30)
+
