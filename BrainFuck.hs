86980300e36b55a20b0d0991e6a19fc2e20105e9
Nikita Hismatov <hismatovns@gmail.com>
2012-02-29 13:47:09 +0600

    using StateT now

--- BrainFuck.hs
+++ BrainFuck.hs
@@ -1,70 +1,78 @@
 module BrainFuck where
 
-import Data.Char
+import Control.Monad.State
 
-data B = B { pos :: Int, cells :: [Int] } deriving (Show)
-
-size :: B -> Int
-size b = length $ cells b
-
-fwd :: B -> IO B
-fwd b
-    | pos b < size b = return $ B (1 + pos b) (cells b)
-    | otherwise      = return $ B 0 (cells b)
-
-back :: B -> IO B
-back b
-    | pos b > 0 = return $ B (-1 + pos b) (cells b)
-    | otherwise = return $ B (-1 + size b) (cells b)
-
-get_curr :: B -> Int
-get_curr b = (cells b) !! (pos b)
-
-set_curr :: B -> Int -> B
-set_curr b val = B n ((take n xs) ++ [val] ++ (drop (n + 1) xs))
-                    where xs = cells b
-                          n  = pos b
-
-inc :: B -> IO B
-inc b = return $ set_curr b new_val
-            where new_val = 1 + (get_curr b)
-
-dec :: B -> IO B
-dec b = return $ set_curr b new_val
-            where new_val = -1 + (get_curr b)
-
-output :: B -> IO B
-output b = (putStr [(chr $ (cells b) !! (pos b))]) >> return b
-
-input :: B -> IO B
-input b = (getLine >>= return . ord . head) >>= return . set_curr b
-
-noop :: B -> IO B
-noop b = return b
-
-command :: Char -> (B -> IO B)
-command '+' = inc
-command '-' = dec
-command '>' = fwd
-command '<' = back
-command '.' = output
-command ',' = input
-command _   = noop
+data B = B { 
+    pos :: Int, 
+    cells :: [Int] 
+} deriving (Show)
 
 createB :: Int -> B
 createB n = B 0 (fillzeros(n))
                 where fillzeros n = take n $ repeat 0
 
-eval_helper :: Int -> [Char] -> B -> IO B
-eval_helper n ops b = if (n + 1) > length(ops)
-                        then 
-                            return b
-                        else
-                            current_op b >>= eval_helper (n + 1) ops
-                                   where current_op = command $ ops !! n
+size :: B -> Int
+size b = length $ cells b
 
-eval :: [Char] -> IO B
-eval [] = return $ createB 30
-eval s  = eval_helper 0 s (createB 30)
+main :: IO ()
+main = runStateT code (createB 5) >> return ()
+
+get_curr :: B -> Int
+get_curr b = (cells b) !! (pos b)
 
 
+
+apply :: (Int -> Int) -> StateT B IO ()
+apply f = do
+            b <- get
+            put $ let n = pos b
+                      h = take n $ cells b -- head
+                      t = drop (n + 1) $ cells b -- tail
+                      val = f $ get_curr b
+                      in B n $ h ++ [val] ++ t
+            return ()
+
+fwd_helper :: B -> B
+fwd_helper b
+    | pos b < -1 + size b = B (1 + pos b) (cells b)
+    | otherwise           = B 0 (cells b)
+
+fwd :: StateT B IO ()
+fwd = do
+        b <- get
+        put $ fwd_helper b
+        return ()
+
+back_helper :: B -> B
+back_helper b
+        | pos b > 0 = B (-1 + pos b) (cells b)
+        | otherwise = B (-1 + size b) (cells b)
+
+back :: StateT B IO ()
+back = do
+        b <- get
+        put $ back_helper b
+        return ()
+
+output :: StateT B IO ()
+output = do
+    b <- get
+    liftIO $ print $ get_curr b
+    liftIO $ print $ show b
+    return ()
+
+code :: StateT B IO ()
+code = do
+    apply (+10)
+    apply (+20)
+    output
+
+    back
+    apply (+1)
+    output
+    
+    fwd
+    apply (+2)
+    output
+
+    return ()
